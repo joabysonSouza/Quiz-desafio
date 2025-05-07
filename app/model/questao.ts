67c8bee2bf0a4cdac8df4d4e4b56264ac3764d15
Joabyson <joabyson916@gmail.com>
2025-05-06 22:09:37 -0300

    Feat: "Adicionado a função para converter para objetos "

--- app/model/questao.ts
+++ app/model/questao.ts
@@ -40,5 +40,18 @@ export default class QuestaoModel{
         }
         return false
     }
+    
+    converterParaObjeto(){
+        return{
+            id : this.#id ,
+            enunciado : this.#enunciado,
+            respostas :this.#respostas.map(resp => resp.converterParaObjeto()),
+            acertou: this.#acertou,
+
+        } 
+           
+
+        
+    }
 }
 
