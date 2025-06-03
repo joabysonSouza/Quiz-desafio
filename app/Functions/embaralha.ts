b2da170dfdffc4e35d1a2732622c732b0e792d97
Joabyson <joabyson916@gmail.com>
2025-06-02 22:49:32 -0300

    Feat: "Criando função para embaralha os ids"

---
+++ app/Functions/embaralha.ts
@@ -0,0 +1,8 @@
+
+
+export default function embaralha(arr:number[]) {
+
+return arr.sort(() => Math.random() - 0.5);
+
+
+}
--- app/api/questoes/[id]/route.ts
+++ app/api/questoes/[id]/route.ts
@@ -1,15 +1,21 @@
+
 import { NextRequest, NextResponse } from "next/server";
 import questoes from "../../bancoQuestoes"
+import embaralha from "@/app/Functions/embaralha";
+
 
 export async function GET(req: NextRequest,  {params}: {params:{id: string}}) {
-    const indice = Number(params.id)
+    // const indice = Number(params.id)
 
-    const questao = questoes.find(questaoId => questaoId.id == indice)
+    // const questao = questoes.find(questaoId => questaoId.id == indice)
 
-    if(!questao){
-        return NextResponse.json({ erro: "Questão não encontrada" }, { status: 204 })
-    }
+    // if(!questao){
+    //     return NextResponse.json({ erro: "Questão não encontrada" }, { status: 204 })
+    // }
+   const ids = questoes.map(ques => ques.id)
 
-    return NextResponse.json(questoes[Number(indice)].converterParaObjeto())
+   const teste = embaralha(ids)
+
+    return NextResponse.json(teste)
 }
 
