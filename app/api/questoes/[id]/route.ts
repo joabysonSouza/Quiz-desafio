9468535f564d381d71fe39ff550723cbe323b40f
Joabyson <joabyson916@gmail.com>
2025-07-07 22:52:38 -0300

    Feat:"começando a lógica de  embaralha as respostas "

--- app/api/questoes/[id]/route.ts
+++ app/api/questoes/[id]/route.ts
@@ -1,21 +1,15 @@
-
 import { NextRequest, NextResponse } from "next/server";
-import questoes from "../../bancoQuestoes"
-import embaralha from "@/app/Functions/embaralha";
+import questoes from "../../bancoQuestoes";
 
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const indice = Number(params.id);
 
-export async function GET(req: NextRequest,  {params}: {params:{id: string}}) {
-    // const indice = Number(params.id)
+  const questaoSelecionada = questoes.find(questao => questao.id === indice);
 
-    // const questao = questoes.find(questaoId => questaoId.id == indice)
-
-    // if(!questao){
-    //     return NextResponse.json({ erro: "Questão não encontrada" }, { status: 204 })
-    // }
-   const ids = questoes.map(ques => ques.id)
-
-   const teste = embaralha(ids)
-
-    return NextResponse.json(teste)
+  if (questaoSelecionada) {
+    const questaoEmbaralhada = questaoSelecionada.embaralharespostas();
+    return NextResponse.json(questaoEmbaralhada.converterParaObjeto());
+  } else {
+    return NextResponse.json({ erro: "Questão não encontrada" }, { status: 404 });
+  }
 }
-
