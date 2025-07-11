7d1a82fe5f3e92a3ccd1193b56530dfb50bcfd1a
Joabyson <joabyson916@gmail.com>
2025-07-10 22:02:23 -0300

    Feat: "criando o  primeiro componente visual do quiz "

---
+++ app/Components/questao.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import QuestaoModel from '../model/questao'
+
+
+interface TypeProps {
+    enunciado : QuestaoModel
+
+}
+
+const Questao = (enunciado: TypeProps) => {
+
+
+  return (
+    <div>
+        <h1>Ola</h1>
+      
+    </div>
+  )
+}
+
+export default Questao
