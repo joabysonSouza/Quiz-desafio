42125ec38ae70ff19b4d5379cd226393f385b5ab
Joabyson <joabyson916@gmail.com>
2025-07-21 22:01:07 -0300

    Refactor: "removendo espaços em brancos"

--- app/api/questoes/[id]/route.ts
+++ app/api/questoes/[id]/route.ts
@@ -1,14 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import questoes from "../../bancoQuestoes";
 
+
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const indice = Number(params.id);
 
-  const questaoSelecionada = questoes.find(questao => questao.id === indice);
+   const questaoSelecionada = questoes.find(questao => questao.id === indice);
 
   if (questaoSelecionada) {
-    const questaoEmbaralhada = questaoSelecionada.embaralharespostas();
-    return NextResponse.json(questaoEmbaralhada.converterParaObjeto());
+    const questaoEmbaralhada = questaoSelecionada.embaralhaRespostas();
+    
+    return  NextResponse.json(questaoEmbaralhada.converterParaObjeto())
   } else {
     return NextResponse.json({ erro: "Questão não encontrada" }, { status: 404 });
   }
